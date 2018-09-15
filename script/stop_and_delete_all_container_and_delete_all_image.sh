28e363bd61c6f3786089c69484b899c35cd569fe
Toshifumi Kurosawa <kuromailserver@gmail.com>
2018-09-15 18:44:09 +0000

    dockerfileからkurosawakuro/ruby_on_rails_image«変更

--- script/stop_and_delete_all_container_and_delete_all_image.sh
+++ script/stop_and_delete_all_container_and_delete_all_image.sh
@@ -1,1 +1,40 @@
-docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker image rm $(docker image ls -q)
+#!/bin/bash
+
+# デバッグを開始する
+# set -x
+
+echo -e $'\e[1;31m ================================= \e[m'
+echo -e $'\e[1;31m docker ps -a -q \e[m'
+echo -e $'\e[1;31m ================================= \e[m'
+docker ps -a -q
+
+echo -e $'\e[1;31m ================================= \e[m'
+echo -e $'\e[1;31m docker image ls -q \e[m'
+echo -e $'\e[1;31m ================================= \e[m'
+docker image ls -q
+
+echo -e $'\e[1;31m ================================= \e[m'
+echo -e $'\e[1;31m コンテナを停止 \e[m'
+echo -e $'\e[1;31m ================================= \e[m'
+docker stop $(docker ps -a -q)
+
+echo -e $'\e[1;31m ================================= \e[m'
+echo -e $'\e[1;31m コンテナを削除 \e[m'
+echo -e $'\e[1;31m ================================= \e[m'
+docker rm $(docker ps -a -q)
+
+echo -e $'\e[1;31m ================================= \e[m'
+echo -e $'\e[1;31m イメージを削除 \e[m'
+echo -e $'\e[1;31m ================================= \e[m'
+docker image rm $(docker image ls -q)
+
+echo -e $'\e[1;31m ================================= \e[m'
+echo -e $'\e[1;31m docker ps -a -q \e[m'
+echo -e $'\e[1;31m ================================= \e[m'
+docker ps -a -q
+
+echo -e $'\e[1;31m ================================= \e[m'
+echo -e $'\e[1;31m docker image ls -q \e[m'
+echo -e $'\e[1;31m ================================= \e[m'
+docker image ls -q
+
